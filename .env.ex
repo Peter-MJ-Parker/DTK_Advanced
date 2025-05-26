13e105ac6669450795457522226f5bde46300995
George Needham <34216187+Peter-MJ-Parker@users.noreply.github.com>
2025-05-26 18:18:20 -0500

    add config files

---
+++ .env.ex
@@ -0,0 +1,8 @@
+DISCORD_TOKEN=YOUR_DISCORD_TOKEN
+APP_ID=CLIENT_APP_ID
+MONGO_DB_URL=mongodb://localhost:27017/your_database
+DEV_GUILD_ID=123456789
+BOT_OWNER_ID=123456789
+
+# Legacy Command Prefix - Leave blank to disable message commands
+PREFIX=!
